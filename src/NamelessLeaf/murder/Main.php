2945d7f6ce5f1e084361928c26ae18df6a114086
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-15 17:13:50 +0000

    added code

---
+++ src/NamelessLeaf/murder/Main.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace NamelessLeaf\Murder;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
+
+
+class Main extends PluginBase implements Listener {
+  
+  public function onEnable(){
+    $this->getLogger()->info("Enabled");
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info("Disabled");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, String $lable, Array $args) : bool {
+    
+    switch($cmd->getName()){
+      case "playmurder":
+        if($sender instanceof Player){
+          $this->form($sender);
+        }else{
+          $sender->sendMessage("stupid console u cant use form");
+        }
+    }
+    return true;
+  }
+  
+  public function form($player){
+    $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+    $form = $api->createSimpleForm(function (Player $player, int $data = null){
+      $result = $data;
+      if($result === null){
+        return true;
+      }
+      switch($result){
+        case 0:
+          $player->transfer("us1.falixnodes.net", 56466);
+        break;
+          
+        case 1:
+         $player->sendMessage("Exit Succsessful");
+        break;
+    
+      }
+    });
+    $form->setTitle("§e>>§4§lMurder Mystery§r§e<<");
+    $form->setContent("Click To Play Murder Mystery!");
+    $form->addButton("§l§cNormal");
+    $form->addButton("§l§4Extreme");
+    $form->addButton("Exit")
+    $form->sendToPlayer($player);
+    return $form;
+  }
+}
